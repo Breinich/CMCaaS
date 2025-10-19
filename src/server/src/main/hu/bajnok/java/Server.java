811f3b5f03d0827e84a7de753430e1a83e4141ae
Breinich <vencel@bajnok.hu>
2025-10-19 15:32:45 +0200

    add proxy server's entry point

---
+++ src/server/src/main/hu/bajnok/java/Server.java
@@ -0,0 +1,12 @@
+package hu.bajnok.java;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) {
+        run(Server.class, args);
+    }
+}
