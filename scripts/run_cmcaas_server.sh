8c999cdd674559245fa8bf5987ae97448500af41
Breinich <vencel@bajnok.hu>
2025-10-22 10:57:04 +0200

    add helper scripts

---
+++ scripts/run_cmcaas_server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+IMAGE_NAME="cmcaas-server:latest"
+
+if [ -e /dev/sgx_enclave ] && [ -e /dev/sgx_provision ]; then
+    echo "[+] Detected SGX devices, running in non-privileged mode..."
+    docker run -it --device /dev/sgx/enclave --device /dev/sgx/provision $IMAGE_NAME
+else
+    echo "[!] SGX device files not found. Running in privileged fallback mode..."
+    docker run -it --privileged -v /dev/sgx:/dev/sgx $IMAGE_NAME
+fi
