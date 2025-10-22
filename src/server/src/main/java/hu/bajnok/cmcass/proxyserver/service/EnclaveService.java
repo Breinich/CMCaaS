8fb314941769d2d1375d6a8a187c7d23f7bdea11
Breinich <vencel@bajnok.hu>
2025-10-22 14:40:46 +0200

    update logging

--- src/server/src/main/java/hu/bajnok/cmcass/proxyserver/service/EnclaveService.java
+++ src/server/src/main/java/hu/bajnok/cmcass/proxyserver/service/EnclaveService.java
@@ -1,6 +1,5 @@
 package hu.bajnok.cmcass.proxyserver.service;
 
-import hu.bajnok.cmcass.proxyserver.controller.VerifierController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
