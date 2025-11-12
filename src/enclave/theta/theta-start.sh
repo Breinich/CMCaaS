93d9be0c419ad0537c72c0d66b58bbec9d7e2388
Breinich <vencel@bajnok.hu>
2025-11-12 21:13:24 +0100

    update commands

--- src/enclave/theta/theta-start.sh
+++ src/enclave/theta/theta-start.sh
@@ -20,17 +20,9 @@ export VERIFIER_NAME=Theta
 export VERIFIER_VERSION=6.8.6
 
 if [ "$1" == "--version" ]; then
-    LD_LIBRARY_PATH=$scriptdir/lib java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar --version || echo $VERIFIER_VERSION
+    LD_LIBRARY_PATH=$scriptdir/lib:${LD_LIBRARY_PATH} java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar --version || echo $VERIFIER_VERSION
     exit
 fi
-
-JAVA_VERSION=17
-JAVA_FALLBACK_PATH="/usr/lib/jvm/java-$JAVA_VERSION-openjdk-amd64/bin/:/usr/lib/jvm/java-$JAVA_VERSION-openjdk/bin/:/usr/lib/jvm/java-$JAVA_VERSION/bin/"
-grep -o "openjdk $JAVA_VERSION" <<< "$(java --version)" >/dev/null || export PATH="$JAVA_FALLBACK_PATH":$PATH
-grep -o "openjdk $JAVA_VERSION" <<< "$(java --version)" >/dev/null || {
-    echo "Could not set up openjdk-$JAVA_VERSION. Is the JRE/JDK installed?"
-    exit 1
-}
 
 remove_property() {
     local args=()
@@ -54,33 +46,11 @@ modified_args=$(remove_property "${@:2}")
 property=$(cat .property && rm .property)
 echo "Verifying input '$IN' with property '$property' using arguments '$modified_args'"
 
-if [ "$(basename "$property")" == "termination.prp" ]; then
-    transformed_property=$(dirname "$property")/unreach-call.prp
-    echo "Mapping property '$property' to '$transformed_property'"
-    TMPFILE=$(mktemp -p "$PWD")
-    sed 's/__VERIFIER_assert/__OLD_VERIFIER_assert/g;s/reach_error/old_reach_error/g' "$IN" > "$TMPFILE"
-    python3 "$scriptdir"/specification-transformation/src/specification-transformation.py --from-property termination --to-property reachability --algorithm InstrumentationOperator "$TMPFILE"
-    #"$scriptdir"/offset.sh "$IN" "output/transformed_program.c" > witness-mapping.yml
-    modified_args="$modified_args --input-file-for-witness $IN"
-    IN="output/transformed_program.c"
-    rm "$TMPFILE"
-elif [ "$(basename "$property")" == "no-overflow.prp" ]; then
-    transformed_property=$(dirname "$property")/unreach-call.prp
-    echo "Mapping property '$property' to '$transformed_property'"
-    TMPFILE=$(mktemp -p "$PWD")
-    sed 's/__VERIFIER_assert/__OLD_VERIFIER_assert/g;s/reach_error/old_reach_error/g' "$IN" > "$TMPFILE"
-    python3 "$scriptdir"/specification-transformation/src/specification-transformation.py --from-property no-overflow --to-property reachability --algorithm InstrumentationOperator "$TMPFILE"
-    #"$scriptdir"/offset.sh "$IN" "output/transformed_program.c" > witness-mapping.yml
-    modified_args="$modified_args --input-file-for-witness $IN"
-    IN="output/transformed_program.c"
-    rm "$TMPFILE"
-else
-    transformed_property="$property"
-fi
+transformed_property="$property"
 
 echo "Using Java heap -Xmx=${XMX} (${XMX_REASON})"
-echo LD_LIBRARY_PATH="$scriptdir"/lib java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar "$modified_args" --input "$IN" --property "$transformed_property" --smt-home "$scriptdir"/solvers
-LD_LIBRARY_PATH="$scriptdir"/lib java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar "$modified_args" --input "$IN" --property "$transformed_property" --smt-home "$scriptdir"/solvers
+echo LD_LIBRARY_PATH="$scriptdir"/lib:"${LD_LIBRARY_PATH}" java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar "$modified_args" --input "$IN" --property "$transformed_property" --smt-home "$scriptdir"/solvers
+LD_LIBRARY_PATH="$scriptdir"/lib:"${LD_LIBRARY_PATH}" java -Xmx"${XMX}" -Djdk.lang.Process.launchMechanism=posix_spawn -jar "$scriptdir"/theta.jar "$modified_args" --input "$IN" --property "$transformed_property" --smt-home "$scriptdir"/solvers
 
 if [ "$(basename "$property")" == "termination.prp" ]; then
     echo "Not yet mapping witnesses from '$transformed_property' to '$property', hoping for the best"
