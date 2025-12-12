202fa7ed2ccc9d61230f8d5ef2d4c5a93e259934
Breinich <vencel@bajnok.hu>
2025-12-12 12:28:42 +0100

    name the containers

--- scripts/run_cmcaas_server.sh
+++ scripts/run_cmcaas_server.sh
@@ -5,8 +5,8 @@ IMAGE_NAME="cmcaas-server"
 
 if [ -e /dev/sgx_enclave ] && [ -e /dev/sgx_provision ]; then
     echo "[+] Detected SGX devices, running in non-privileged mode..."
-    docker run -it --device /dev/sgx_enclave --device /dev/sgx_provision -p 8080:8080 "$IMAGE_NAME:latest"
+    docker run -it --device /dev/sgx_enclave --device /dev/sgx_provision -p 8080:8080 --name "$IMAGE_NAME" "$IMAGE_NAME:latest"
 else
     echo "[!] SGX device files not found. Running in simulation mode..."
-    docker run -it -p 8080:8080 "$IMAGE_NAME:sim"
+    docker run -it -p 8080:8080 --name "$IMAGE_NAME" "$IMAGE_NAME:sim"
 fi
