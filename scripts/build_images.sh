81e3a165541175a0ac5de668a6b34cbe234b75da
Breinich <vencel@bajnok.hu>
2025-12-14 15:48:24 +0100

    update build images script

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -9,7 +9,12 @@ docker build -t "$IMAGE_NAME:latest" .
 echo "[+] Building CMCaaS Simulation Docker image..."
 docker build -f Dockerfile_sim -t "$IMAGE_NAME:sim" .
 
+echo "[+] Building enclave verifier Docker image..."
+docker build -f Dockerfile_verifier -t "cmcaas-verifier:latest" .
+
 echo "[+] CMCaaS Docker images built successfully:"
 echo "    - $IMAGE_NAME:latest"
 echo "    - $IMAGE_NAME:sim"
+echo "    - cmcaas-verifier:latest"
+
 
